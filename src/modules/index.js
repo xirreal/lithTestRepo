e506b18293432c767d4fb86d844388dcabcbb929
xirreal <davi.irreal@gmail.com>
2021-02-24 21:19:04 +0100

    Test

---
+++ src/modules/index.js
@@ -0,0 +1,12 @@
+import localrepo from './localrepo.js';
+
+export default [
+  {
+    meta: {
+      name: "Lith's repo",
+      description: 'Private repo for my (bad) modules.',
+    },
+    filename: 'localrepo',
+    modules: localrepo
+  }
+];
---
+++ src/modules/localrepo.js
@@ -0,0 +1,4 @@
+export default
+[
+  ['xirreal/acrylicSettings', 'master'],
+];
